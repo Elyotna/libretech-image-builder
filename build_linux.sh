b4f72c8935e022c7d2312bab10b80b9dfaf15ed7
Maxime Jourdan <mjourdan@baylibre.com>
2018-11-14 19:14:49 +0100

    kernel: bump to 4.19-stable
    Also add many patches

--- build_linux.sh
+++ build_linux.sh
@@ -1,8 +1,9 @@
-git clone https://github.com/libre-computer-project/libretech-linux.git -b linux-4.14/libretech-cc-master-stable --depth 1 linux-libretech
-cd linux-libretech
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y --depth 1 linux
+cd linux
+git apply ../patches/linux/*.patch
 make ARCH=arm64 defconfig
-#git apply ../patches/0001-meson_drm_disable_ARGB8888.patch
-git apply ../patches/0001-ARM64-dts-meson-gxl-s905x-libretech-cc-Disable-CVBS-.patch
 sed -i 's/CONFIG_BTRFS_FS=m/CONFIG_BTRFS_FS=y/' .config
+sed -i 's/# CONFIG_VIDEO_MESON_VDEC is not set/CONFIG_VIDEO_MESON_VDEC=m/' .config
+sed -i 's/# CONFIG_SQUASHFS_XZ is not set/CONFIG_SQUASHFS_XZ=y/' .config
 PATH=$PWD/../gcc-linaro-6.4.1-2017.08-x86_64_aarch64-linux-gnu/bin:$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4
 cd -
