dffcf3a73c17f15712476f8097212fd08d75bafa
Maxime Jourdan <mjourdan@baylibre.com>
2018-11-23 18:21:33 +0100

    linux: use a tailored defconfig
    Avoid building many unecessary modules that take time, but more
    importantly space on the device

--- build_linux.sh
+++ build_linux.sh
@@ -1,8 +1,6 @@
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y --depth 1 linux
 cd linux
 git apply ../patches/linux/*.patch
-make ARCH=arm64 defconfig
-sed -i 's/# CONFIG_VIDEO_MESON_VDEC is not set/CONFIG_VIDEO_MESON_VDEC=m/' .config
-sed -i 's/# CONFIG_SQUASHFS_XZ is not set/CONFIG_SQUASHFS_XZ=y/' .config
+cp ../defconfig_amlogic_arm64 .config
 PATH=$PWD/../gcc-linaro-6.4.1-2017.08-x86_64_aarch64-linux-gnu/bin:$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4
 cd -
